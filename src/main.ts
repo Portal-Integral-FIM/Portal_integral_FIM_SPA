97076f294ac28b7e7444cd907c20fb6671346379
Pablo <pablo@quadrosoluciones.com>
2023-03-24 15:54:00 -0700

    Init empty project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import routes from '~pages'
+/* Theme variables */
+import './theme/variables.css'
+const pinia = createPinia()
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+})
+
+const app = createApp(App)
+    .use(pinia)
+    .use(router)
+
+router.isReady().then(() => {
+    app.mount('#app')
+})
